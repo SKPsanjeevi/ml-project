fe62148b12e03ea1831a59289046ff1ad2026c50
Sathish Sanjeevi <sathish.krishnan.p.s@gmail.com>
2021-03-03 23:35:47 -0500

    moved file

---
+++ stock.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, svm
+#from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+quandl.ApiConfig.api_key =  "-mEZpeg3QHmxA4fn8pKY"
+df = quandl.get("WIKI/GOOGL")
+
+print(df.head())
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+print(df.head())
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'], 1))
+y = np.array(df['label'])
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+clf = svm.SVR()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+clf = LinearRegression()
+clf = LinearRegression(n_jobs=-1)
+
+for k in ['linear','poly','rbf','sigmoid']:
+    clf = svm.SVR(kernel=k)
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print(k,confidence)
+
+print("test")
